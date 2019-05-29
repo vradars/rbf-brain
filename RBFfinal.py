993f1df701667be656db606e446f2736cee1c4b1
Reuben Kraft <reuben.kraft@gmail.com>
2019-05-29 12:01:47 -0400

    converted notebook to python file

---
+++ RBFfinal.py
@@ -0,0 +1,33 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+#get_ipython().run_line_magic('matplotlib', 'inline')
+from pygem import RBFParameters, RBF, StlHandler, VtkHandler
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# In[2]:
+
+
+params = RBFParameters()
+params.read_parameters(filename='../tests/test_datasets/parameters_rbf_custom.prm')
+
+
+# In[2]:
+
+
+vtk_handler = VtkHandler()
+mesh = vtk_handler.parse('../tests/test_datasets/initialmesh.vtk')
+
+
+# In[4]:
+
+
+rbf = RBF(params, mesh)
+rbf.perform()
+new_mesh_points = rbf.modified_mesh_points
+vtk_handler.write(new_mesh_points, "finalmesh.vtk")
