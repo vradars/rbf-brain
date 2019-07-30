cbb61c8e6845337455e5701f09ea2e80f9c216d0
Reuben Kraft <reuben.kraft@gmail.com>
2019-07-30 13:55:03 -0400

    Update RBFfinal.py

--- RBFfinal.py
+++ RBFfinal.py
@@ -21,7 +21,7 @@ params.read_parameters(filename='../tests/test_datasets/parameters_rbf_custom.pr
 
 
 vtk_handler = VtkHandler()
-mesh = vtk_handler.parse('../tests/test_datasets/initialmesh.vtk')
+mesh = vtk_handler.parse('../tests/test_datasets/Merged.vtk')
 
 
 # In[4]:
@@ -30,4 +30,4 @@ mesh = vtk_handler.parse('../tests/test_datasets/initialmesh.vtk')
 rbf = RBF(params, mesh)
 rbf.perform()
 new_mesh_points = rbf.modified_mesh_points
-vtk_handler.write(new_mesh_points, "finalmesh.vtk")
+vtk_handler.write(new_mesh_points, "morphed_mesh.vtk")
