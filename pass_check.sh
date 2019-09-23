52663c567d700d248bf9799369bae5bfe0907d39
Reuben Kraft <reuben.kraft@gmail.com>
2019-09-23 06:24:04 -0400

    Create pass_check.sh

---
+++ pass_check.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Add file and the target file size below
+repo=PyGem
+file=finalmesh.vtk
+target_file_size=53295
+
+# You shoud not have to modify below
+#
+myfilesize=$(wc -c <"$file")
+echo Acutal File Size = "$myfilesize"
+echo Target File Size = "$target_file_size"
+
+if [ $myfilesize -ge $target_file_size ];then
+        echo Passed!
+        echo "Passed" >> ~/$repo.PASSED
+        echo "Acutal File Size = "$myfilesize" " >> ~/$repo.PASSED
+        echo "Target File Size = "$target_file_size" " >> ~/$repo.PASSED
+else
+        echo Failed!
+        echo "Failed!" >> ~/$repo.FAILED
+        echo "Acutal File Size = "$myfilesize" " >> ~/$repo.FAILED
+        echo "Target File Size = "$target_file_size" " >> ~/$repo.FAILED
+fi
